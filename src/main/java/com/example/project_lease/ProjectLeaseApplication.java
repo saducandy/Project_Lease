cff45fe67849ce16b59c6297e5cc9e5339a8065c
saducandy <adoniasandarge@gmail.com>
2023-05-17 08:57:05 +0100

    Creating Project Lease!

---
+++ src/main/java/com/example/project_lease/ProjectLeaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.project_lease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectLeaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectLeaseApplication.class, args);
+    }
+
+}
